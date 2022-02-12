cd18a91c6a96c5158d8c763816166116fd37dbfe
Benis Tambe <will.jenifer@yahoo.com>
2022-02-12 08:00:29 -0500

    Mounted new components in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import Navigation from './components/Navigation'
+import Home from './components/Home'
+import About from './components/About'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <Navigation />
       </header>
+
+      <Home />
+      
+
     </div>
   );
 }
