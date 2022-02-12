aa55cfc7fe9a3c54b6e28761f6e1471f192964dd
Natasha Koller <kollernatasha@gmail.com>
2022-02-12 09:45:53 -0500

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
 
     render() { return (
         <>
-            <h1>  This is the Footer </h1>
+            <h1>  This is the Footer It is CUTEEEEEE</h1>
         </>
     )}
 
