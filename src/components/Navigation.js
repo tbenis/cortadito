0620c56882a43f2da3ce122dfcf2ecae3c73bae8
Natasha Koller <kollernatasha@gmail.com>
2022-02-12 18:05:26 -0500

    stuff

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,20 +8,27 @@ export default class Navigation extends Component {
 
     render() {
         return (
-        
-           <div>
-            <Navbar className='nav-bar'style={{ minWidth: 700}}>
-					<Navbar.Brand className='brand' href="/">Cortadito</Navbar.Brand>
-					<Nav className="mr-auto" style={{marginLeft: "auto", paddingRight: 40 }}>
-						<Nav.Link href="/">Home</Nav.Link>
-						<Nav.Link href="about">About</Nav.Link>
-						<Nav.Link href="mental-health">Mental Health</Nav.Link>
-                        <Nav.Link href="legal">Legal Services</Nav.Link>
-                        <Nav.Link href="signUp">Sign Up</Nav.Link>
-					</Nav>
-                </Navbar>
-            </div>
-                    
-        )
+          <div>
+            <Navbar className="nav-bar" style={{ minWidth: 700 }}>
+              <img
+                id="coffee"
+                src="https://previews.123rf.com/images/fokaspokas/fokaspokas1803/fokaspokas180300007/96756525-cup-of-hot-coffee-icon-on-transparent-background-.jpg"
+              ></img>
+              <Navbar.Brand className="brand" href="/">
+                Cortadito
+              </Navbar.Brand>
+              <Nav
+                className="mr-auto"
+                style={{ marginLeft: "auto", paddingRight: 40 }}
+              >
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="about">About</Nav.Link>
+                <Nav.Link href="mental-health">Mental Health</Nav.Link>
+                <Nav.Link href="legal">Legal Services</Nav.Link>
+                <Nav.Link href="signUp">Sign Up</Nav.Link>
+              </Nav>
+            </Navbar>
+          </div>
+        );
     }
 }
