6eacd5b05993b39497a5f2b21c54d353096e6f92
Natasha Koller <kollernatasha@gmail.com>
2022-02-12 10:34:49 -0500

    about info added

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,30 @@ import react, { Component } from 'react'
 export default class About extends Component {
 
     render() { return (
-        <>
-            <h1>  This is the About Page </h1>
-        </>
-    )}
+      <>
+        <h1>About Cortadito</h1>
+          <p>
+            About 71% of Miami’s population identifies as Latin American, but
+            just what is a Latin American? What makes up a Latin American? Well,
+            much like Miami’s signature coffee, “el cortadito”, it varies. Sure,
+            the ingredients may be similar, however, how they are expressed is
+            what brings about the uniqueness of every Latin American individual.
+            The parts that make the Latinx community so diverse are also the
+            ones that bring the community together. However, it’s not without
+            its challenges. When people emmigrate from Latin America to the U.S.
+            they bring with them their culture and language. Preserving those
+            traditions and finding their identities in their new homes can be a
+            challenge for many immigrants. Whether the circumstances of their
+            arrival is planned or forced, lawfully or unlawfully, moving to a
+            foreign land with a new language can be very stressful. It is in
+            familiarity that we find comfort, which could be why that percentage
+            is so high for Miami. But what if we could help our residentes of
+            Miami to prepare them to integrate with the remaining 29% of its
+            people? That’s what Cortadito aims to do! Our App is meant to
+            connect people from Latin-America to resources available in Miami as
+            well as people in the community.
+          </p>
+      </>
+    );}
 
 }
