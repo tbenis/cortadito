9c8955f56963f947175a21a5c38d75e06b552def
Jennifer Tchai <jentchai@gmail.com>
2022-02-12 11:51:11 -0500

    added table to mental health

--- src/components/MentalHealth.js
+++ src/components/MentalHealth.js
@@ -13,48 +13,48 @@ export default class Mentalhealth extends Component {
           </tr>
           <tr>
             <th>Florida Blue Helpline</th>
-            <th>Bilingual 24/7 Free therapy for all</th>
-            <th>(833) 848-1762 - https://www.floridablue.com/blog/support-line?fbclid=IwAR0JaIo9qoGRV6A4rTnuKANFbaJJy3VUsX7kwxqyJix6MXNMU5L-RCNqH1c</th>
+            <th>Bilingual 24/7 free therapy for all</th>
+            <th>1(833) 848-1762 - https://www.floridablue.com/blog/support-line?fbclid=IwAR0JaIo9qoGRV6A4rTnuKANFbaJJy3VUsX7kwxqyJix6MXNMU5L-RCNqH1c</th>
           </tr>
           <tr>
             <th>Optum</th>
-            <th>24/7 Free therapy for all</th>
-            <th>(866) 342-6892</th>
+            <th>24/7 free and confidential therapy for all</th>
+            <th>1(866) 342-6892</th>
           </tr>
           <tr>
-            <th>Florida Blue Helpline</th>
-            <th>Tim Berners-Lee</th>
-            <th>833-848-1762 - https://www.floridablue.com/blog/support-line?fbclid=IwAR0JaIo9qoGRV6A4rTnuKANFbaJJy3VUsX7kwxqyJix6MXNMU5L-RCNqH1c</th>
+            <th>National Alliance on Mental Illness(NAMI)</th>
+            <th>Support, education, and advocacy resources for people with mental illness</th>
+            <th>http://www.nami.org</th>
           </tr>
           <tr>
-            <th>Florida Blue Helpline</th>
-            <th>Tim Berners-Lee</th>
-            <th>833-848-1762 - https://www.floridablue.com/blog/support-line?fbclid=IwAR0JaIo9qoGRV6A4rTnuKANFbaJJy3VUsX7kwxqyJix6MXNMU5L-RCNqH1c</th>
+            <th>Thriving Mind/South Florida Behavioral Health Network</th>
+            <th>Promotes access to individuals and families including uninsured individuals in Miami-Dade and Monroe counties</th>
+            <th>https://www.thrivingmind.org/</th>
           </tr>
           <tr>
-            <th>Florida Blue Helpline</th>
-            <th>Tim Berners-Lee</th>
-            <th>833-848-1762 - https://www.floridablue.com/blog/support-line?fbclid=IwAR0JaIo9qoGRV6A4rTnuKANFbaJJy3VUsX7kwxqyJix6MXNMU5L-RCNqH1c</th>
+            <th>National Suicide Hotline</th>
+            <th>24/7 hotline for free and confidential support for people in distress, prevention, and crisis resources</th>
+            <th>1-800-273-8255 https://suicidepreventionlifeline.org/</th>
           </tr>
           <tr>
-            <th>Florida Blue Helpline</th>
-            <th>Tim Berners-Lee</th>
-            <th>833-848-1762 - https://www.floridablue.com/blog/support-line?fbclid=IwAR0JaIo9qoGRV6A4rTnuKANFbaJJy3VUsX7kwxqyJix6MXNMU5L-RCNqH1c</th>
+            <th>National Domestic Violence</th>
+            <th>24/7 hotline for victims of domestic violence</th>
+            <th>1-800-962-2873</th>
           </tr>
           <tr>
-            <th>Florida Blue Helpline</th>
-            <th>Tim Berners-Lee</th>
-            <th>833-848-1762 - https://www.floridablue.com/blog/support-line?fbclid=IwAR0JaIo9qoGRV6A4rTnuKANFbaJJy3VUsX7kwxqyJix6MXNMU5L-RCNqH1c</th>
+            <th>Teen Crisis Hotline</th>
+            <th>24/7 hotline for teens</th>
+            <th>1(800) 448-3000</th>
           </tr>
           <tr>
-            <th>Florida Blue Helpline</th>
-            <th>Tim Berners-Lee</th>
-            <th>833-848-1762 - https://www.floridablue.com/blog/support-line?fbclid=IwAR0JaIo9qoGRV6A4rTnuKANFbaJJy3VUsX7kwxqyJix6MXNMU5L-RCNqH1c</th>
+            <th>Transgender Suicide Hotline</th>
+            <th>24/7 hotline for transgendered community</th>
+            <th>(877) 565-8860</th>
           </tr>
           <tr>
-            <th>Florida Blue Helpline</th>
-            <th>Tim Berners-Lee</th>
-            <th>833-848-1762 - https://www.floridablue.com/blog/support-line?fbclid=IwAR0JaIo9qoGRV6A4rTnuKANFbaJJy3VUsX7kwxqyJix6MXNMU5L-RCNqH1c</th>
+            <th>Mental Health Telehealth/Counseling Services at the FSU</th>
+            <th>Remotely attend counseling and psychiatric services from your home</th>
+            <th>https://uhs.fsu.edu/telehealth-televisits</th>
           </tr>
           </tbody>
         </table>
