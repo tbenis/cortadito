58a7c0e4b19f9c397b5670f2c6f253343e1e660e
Jennifer Tchai <jentchai@gmail.com>
2022-02-12 16:41:44 -0500

    added legal resources

--- src/components/Legal.js
+++ src/components/Legal.js
@@ -5,9 +5,11 @@ export default class Legal extends Component {
     render() { return (
     <>
     <h1>LEGAL SERVICES</h1>
+    <p>Miami-Dade residents and immigrants could face risks of being racially profiled and being detained without probable cause.</p>
     <p>For immigrants who arrive without documentation, the fear of deportation can prevent them from seeking help. Even though millions of children of undocumented immigrants are eligible for health insurance under the Affordable Care Act, many families either may not know about the eligibility or be afraid to register due to fear of separation.</p>
+    <p>Are you a Miami-Dade Latinx immigrant or resident needing legal services?</p>
 
-    <h1>Bilingual Psychiatrists</h1>
+    <h1>Immigration Services Providers</h1>
     <table>
         <tbody>
           <tr>
@@ -16,34 +18,34 @@ export default class Legal extends Component {
             <th>Contact Info</th>
           </tr>
           <tr>
-            <th>Dr. Gonzalo F. Quesada, MD</th>
-            <th>Psychiatrist</th>
+            <th>Center for Immigrant Advancement, Inc. (CIMA)</th>
+            <th>Pro Bono</th>
             <th>100 SE 2nd St STE 2000, Miami, Florida 33131 (305) 416-8811</th>
           </tr>
           <tr>
-            <th>Dr. Robert Stewart, DO</th>
-            <th>Psychiatrist</th>
+            <th>Catholic Charities Legal Services Archdiocese of Palm Beach, Inc.</th>
+            <th>Pro Bono</th>
             <th>7031 SW 62nd Ave South Miami, FL 33143 (305) 330-1534</th>
           </tr>
           <tr>
-            <th>Dr. Vineet Sandhu, MD</th>
-            <th>Psychiatrist</th>
+            <th>Florida Justice For Our Neighbors</th>
+            <th>Pro Bono</th>
             <th>1000 Brickell Ave Miami, Fl 33130 (908) 852-1887</th>
           </tr>
           <tr>
-            <th>Dr. Maria Adelaida Rueda-Lara, MD</th>
-            <th>Psychiatrist</th>
+            <th>American Bar Association Detention and LOP Info Line</th>
+            <th>Pro se case assistance for detained respondents only</th>
             <th>1121 NW 14th St Floor 1, Miami, FL, 33135 (305) 243-0214</th>
           </tr>
           <tr>
-            <th>Dr. Jose Valldaraes, MD</th>
-            <th>Psychiatrist</th>
+            <th>Americans for Immigrant Justice</th>
+            <th>Pro Bono</th>
             <th>2660 SW 3rd St, Miami, FL 33135 (786) 281-1005</th>
           </tr>
           </tbody>
         </table>
 
-        <h1>Bilingual Therapists</h1>
+        <h1>Other Legal Aid Providers</h1>
     <table>
         <tbody>
           <tr>
@@ -52,100 +54,29 @@ export default class Legal extends Component {
             <th>Contact Info</th>
           </tr>
           <tr>
-            <th>Teresita Lambertt, LMHC</th>
-            <th>Counselor</th>
+            <th>Casa Miami</th>
+            <th>Free legal services to immigrants that have been victims of domestic violence</th>
             <th>292 S University Drive, Miami, FL 33130, (305) 487-8646</th>
           </tr>
           <tr>
-            <th>Olivia Lima, LMHC</th>
-            <th>Counselor</th>
+            <th>Florida Justice Institute Miami Tower</th>
+            <th>Free legal services for prisoners rights, housing discrimination, disability discrimination</th>
             <th>7031 SW 62nd Ave South Miami, FL 33143 (786) 244-6582</th>
           </tr>
           <tr>
-            <th>Carol Quintana, PhD</th>
-            <th>Psychologist</th>
+            <th>Cuban American Bar Association Pro Bono Project</th>
+            <th>Free legal services for Cuban immigrants experiencing work and housing discrimination</th>
             <th>1000 Brickell Ave Miami, Fl 33130 (305) 570-4010</th>
           </tr>
           <tr>
-            <th>Christina Algeciras, MS, LMHC, RYT, CCTP</th>
-            <th>Counselor</th>
+            <th>Americans for Immigrant Justice</th>
+            <th>Free legal services for children program, domestic violence, and human trafficking</th>
             <th>1121 NW 14th St Floor 1, Miami, FL, 33135 (305) 243-0214</th>
           </tr>
           <tr>
-            <th>Isabel Trabanco, LCSW</th>
-            <th>Clinical Social Work/Therapist</th>
+            <th>Dade Legal Aid Put Something Back</th>
+            <th>Free legal services for LGBTQ+</th>
             <th>2660 SW 3rd St, Miami, FL 33135 (786) 281-1005</th>
-          </tr>
-          </tbody>
-        </table>
-
-    <h1>Hotlines and Websites</h1>
-        <table>
-            <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Contact Info</th>
-          </tr>
-          <tr>
-            <th>Florida Blue Helpline</th>
-            <th>Bilingual 24/7 free therapy for all</th>
-            <th>1(833) 848-1762 - https://www.floridablue.com/</th>
-          </tr>
-          <tr>
-            <th>Optum</th>
-            <th>24/7 free and confidential therapy for all</th>
-            <th>1(866) 342-6892</th>
-          </tr>
-          <tr>
-            <th>National Alliance on Mental Illness(NAMI)</th>
-            <th>Support, education, and advocacy resources for people with mental illness</th>
-            <th>http://www.nami.org</th>
-          </tr>
-          <tr>
-            <th>Thriving Mind/South Florida Behavioral Health Network</th>
-            <th>Promotes access to individuals and families including uninsured individuals in Miami-Dade and Monroe counties</th>
-            <th>https://www.thrivingmind.org/</th>
-          </tr>
-          <tr>
-            <th>National Suicide Hotline</th>
-            <th>24/7 hotline for free and confidential support for people in distress, prevention, and crisis resources</th>
-            <th>1-800-273-8255 https://suicidepreventionlifeline.org/</th>
-          </tr>
-          <tr>
-            <th>National Domestic Violence</th>
-            <th>24/7 hotline for victims of domestic violence</th>
-            <th>1-800-962-2873</th>
-          </tr>
-          <tr>
-            <th>Teen Crisis Hotline</th>
-            <th>24/7 hotline for teens</th>
-            <th>1(800) 448-3000</th>
-          </tr>
-          <tr>
-            <th>Transgender Suicide Hotline</th>
-            <th>24/7 hotline for transgendered community</th>
-            <th>(877) 565-8860</th>
-          </tr>
-          <tr>
-            <th>Mental Health Telehealth/Counseling Services at the FSU</th>
-            <th>Remotely attend counseling and psychiatric services from your home</th>
-            <th>https://uhs.fsu.edu/telehealth-televisits</th>
-          </tr>
-          <tr>
-            <th>Alcoholics Anonymous</th>
-            <th>Remotely attend groups and access resources for people in Miami-Dade</th>
-            <th>https://aamiamidade.org/ (online for COVID)</th>
-          </tr>
-          <tr>
-            <th>Narcotics Anonymous</th>
-            <th>Attend groups and access resources for people in Miami-Dade</th>
-            <th>https://www.namiami.org/meetings/ (please check Online Meetings tab for Online COVID Meetings)</th>
-          </tr>
-          <tr>
-            <th>Overeaters Anonymous</th>
-            <th>Attend groups and access resources for people in Miami-Dade</th>
-            <th>https://oa.org/find-a-meeting/?type=0</th>
           </tr>
           </tbody>
         </table>
